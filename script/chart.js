5f6a4a8b15a51c82be9d81e821c4b429bff60e42
Ponderix <80052935+Ponderix@users.noreply.github.com>
2021-07-17 20:22:46 +0200

    Tabspaces
    fixed pathing
    added 4 tab spaces instead of 2
    added favicon

--- script/chart.js
+++ script/chart.js
@@ -1,56 +1,56 @@
 var chart = {
-  drawBar : function(data, graph, results, result_div) {
+    drawBar: function(data, graph, results, result_div) {
 
-     graph.selectAll("rect")
-       .data(results)
-       .enter()
-       .append("rect")
-         .attr("class", function(d, i) {
-           return d[0];
-         })
-         .attr("height", 30)
-         .attr("x", function(d, i) {
-           if (d[0] == "democratic") {
-             return 0;
-           } else {
-             return 1000 - (1000 * results[1][1] / (results[0][1] + results[1][1]));
-           }
-         })
-         .attr("width", function(d, i) {
-           return 1000 * d[1] / (results[0][1] + results[1][1]);
-         })
-         .attr("y", 0);
+        graph.selectAll("rect")
+            .data(results)
+            .enter()
+            .append("rect")
+            .attr("class", function(d, i) {
+                return d[0];
+            })
+            .attr("height", 30)
+            .attr("x", function(d, i) {
+                if (d[0] == "democratic") {
+                    return 0;
+                } else {
+                    return 1000 - (1000 * results[1][1] / (results[0][1] + results[1][1]));
+                }
+            })
+            .attr("width", function(d, i) {
+                return 1000 * d[1] / (results[0][1] + results[1][1]);
+            })
+            .attr("y", 0);
 
-     result_div.selectAll("span")
-        .data(results)
-        .enter()
-        .append("span")
-          .attr("id", function(d, i) {
-             return d[0] + "Result";
-          })
-          .html(function(d, i) {
-            if (d[0] == "democratic") {
-              return `${d[1]}<span>DEMOCRATIC</span>`;
-            } else {
-              return `<span>REPUBLICAN</span>${d[1]}`;
-            }
-          });
+        result_div.selectAll("span")
+            .data(results)
+            .enter()
+            .append("span")
+            .attr("id", function(d, i) {
+                return d[0] + "Result";
+            })
+            .html(function(d, i) {
+                if (d[0] == "democratic") {
+                    return `${d[1]}<span>DEMOCRATIC</span>`;
+                } else {
+                    return `<span>REPUBLICAN</span>${d[1]}`;
+                }
+            });
 
-     graph.append("rect")
-       .attr("id", "majority")
-       .attr("height", 30)
-       .attr("width", 1)
-       .attr("x", 217/434 * 1000)
-       .attr("y", 0);
+        graph.append("rect")
+            .attr("id", "majority")
+            .attr("height", 30)
+            .attr("width", 1)
+            .attr("x", 217 / 434 * 1000)
+            .attr("y", 0);
 
-     var majority_div = result_div.append("div")
-       .attr("class", "majority-div")
+        var majority_div = result_div.append("div")
+            .attr("class", "majority-div")
 
-     majority_div.append("i")
-       .attr("class", "up-arrow");
-     majority_div.append("div")
-       .attr("id", "majority-info")
-       .html("218 for Majority in the House");
+        majority_div.append("i")
+            .attr("class", "up-arrow");
+        majority_div.append("div")
+            .attr("id", "majority-info")
+            .html("218 for Majority in the House");
 
-   },
+    },
 }
--- script/svg.js
+++ script/svg.js
@@ -6,136 +6,136 @@ var choroButton = d3.select("#choropleth_button");
 var marginsContent = d3.select("#margins_content");
 
 var mapSvg = mapContainer.append("svg")
-  .attr("width", 1200)
-  .attr("height", 600)
-  .attr("id", "usa");
+    .attr("width", 1200)
+    .attr("height", 600)
+    .attr("id", "usa");
 var map = mapSvg.append("g")
-  .attr("id", "boundaries");
+    .attr("id", "boundaries");
 
 var chartSvg = chartContainer.append("svg")
-  .attr("width", 1000)
-  .attr("height", 100)
-  .attr("id", "chart");
+    .attr("width", 1000)
+    .attr("height", 100)
+    .attr("id", "chart");
 var graph = chartSvg.append("g")
-  .attr("id", "graph");
+    .attr("id", "graph");
 
 
 var projection = d3.geoMercator()
-  .scale(1000)
-  .rotate([96,-50])
-  .translate([575, 90]);
+    .scale(1000)
+    .rotate([96, -50])
+    .translate([575, 90]);
 var path = d3.geoPath()
-  .projection(projection);
+    .projection(projection);
 
 
 function zoomed(event) {
-  var transform = event.transform;
-  map.attr("transform", transform.toString());
+    var transform = event.transform;
+    map.attr("transform", transform.toString());
 }
 var zoom = d3.zoom()
-  .scaleExtent([0.7, 60])
-  .on("zoom", zoomed);
+    .scaleExtent([0.7, 60])
+    .on("zoom", zoomed);
 mapSvg.call(zoom).on("dblclick.zoom", null);
 
 
 
 
-d3.json("../data/DRA_2022_USA_simplified[4.1].topo.json").then(function (data) {
+d3.json("./data/DRA_2022_USA_simplified[4.1].topo.json").then(function(data) {
 
-  var democratic = [];
-  var republican = [];
-  var districts = data.objects.boundaries.geometries;
+    var democratic = [];
+    var republican = [];
+    var districts = data.objects.boundaries.geometries;
 
-  var seats = [
-    ["democratic"],
-    ["republican"]
-  ];
+    var seats = [
+        ["democratic"],
+        ["republican"]
+    ];
 
-  for (var i = 0; i < districts.length; i++) {
-    if (districts[i].properties.DemPct > districts[i].properties.RepPct) {
-      democratic.push(districts[i]);
-    } else {
-      republican.push(districts[i]);
-    }
-  }
-
-  seats[0].push(democratic.length);
-  seats[1].push(republican.length);
-
-
-
-
-  drawMap(function (d, i) {
-    return choropleths.choroMargins(d);
-  });
-
-  chart.drawBar(data, graph, seats, seatCounter);
-
-  choroButton.on("click", () =>{
-    if (choroButton._groups[0][0].className === "active") {
-
-      map.selectAll("path").remove();
-      drawMap(function (d, i) {
-        return choropleths.choroMargins(d);
-      });
-
-      marginsContent.style("display", "inline");
-      choroButton.attr("class", "inactive");
-
-    } else {
-
-      map.selectAll("path").remove();
-      drawMap(function (d, i) {
-        return choropleths.choroDynamic(d);
-      });
-
-      marginsContent.style("display", "none");
-      choroButton.attr("class", "active");
-
-    }
-  });
-
-
-
-
-  function drawMap(choro) {
-    map.selectAll("path")
-      .data(topojson.feature(data, data.objects.boundaries).features)
-      .enter().append("path")
-       .attr("d", path)
-       .attr("id", function(d, i) {
-        if (d.properties.TotalPop == 737068) {
-          return "Alaska";
+    for (var i = 0; i < districts.length; i++) {
+        if (districts[i].properties.DemPct > districts[i].properties.RepPct) {
+            democratic.push(districts[i]);
         } else {
-          if (d.properties.TotalPop == 712119) {
-            return "Hawaii South";
-          } else {
-            if (d.properties.TotalPop == 709975) {
-              return "Hawaii North";
-            }
-          }
+            republican.push(districts[i]);
         }
-       })
-       .attr("class", function(d, i) {
-         if (d.properties.TotalPop == 712119 || d.properties.TotalPop == 709975) {
+    }
 
-           if (d.properties.DemPct > d.properties.RepPct) {
-             return "district hawaii democratic";
-           } else {
-             return "district hawaii republican";
-           }
+    seats[0].push(democratic.length);
+    seats[1].push(republican.length);
 
-         } else {
 
-           if (d.properties.DemPct > d.properties.RepPct) {
-             return "district democratic";
-           } else {
-             return "district republican";
-           }
 
-         }
-       })
-       .attr("opacity", choro);
-  }
+
+    drawMap(function(d, i) {
+        return choropleths.choroMargins(d);
+    });
+
+    chart.drawBar(data, graph, seats, seatCounter);
+
+    choroButton.on("click", () => {
+        if (choroButton._groups[0][0].className === "active") {
+
+            map.selectAll("path").remove();
+            drawMap(function(d, i) {
+                return choropleths.choroMargins(d);
+            });
+
+            marginsContent.style("display", "inline");
+            choroButton.attr("class", "inactive");
+
+        } else {
+
+            map.selectAll("path").remove();
+            drawMap(function(d, i) {
+                return choropleths.choroDynamic(d);
+            });
+
+            marginsContent.style("display", "none");
+            choroButton.attr("class", "active");
+
+        }
+    });
+
+
+
+
+    function drawMap(choro) {
+        map.selectAll("path")
+            .data(topojson.feature(data, data.objects.boundaries).features)
+            .enter().append("path")
+            .attr("d", path)
+            .attr("id", function(d, i) {
+                if (d.properties.TotalPop == 737068) {
+                    return "Alaska";
+                } else {
+                    if (d.properties.TotalPop == 712119) {
+                        return "Hawaii South";
+                    } else {
+                        if (d.properties.TotalPop == 709975) {
+                            return "Hawaii North";
+                        }
+                    }
+                }
+            })
+            .attr("class", function(d, i) {
+                if (d.properties.TotalPop == 712119 || d.properties.TotalPop == 709975) {
+
+                    if (d.properties.DemPct > d.properties.RepPct) {
+                        return "district hawaii democratic";
+                    } else {
+                        return "district hawaii republican";
+                    }
+
+                } else {
+
+                    if (d.properties.DemPct > d.properties.RepPct) {
+                        return "district democratic";
+                    } else {
+                        return "district republican";
+                    }
+
+                }
+            })
+            .attr("opacity", choro);
+    }
 
 });
